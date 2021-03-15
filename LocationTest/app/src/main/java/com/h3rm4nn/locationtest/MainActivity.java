c2664b506a02ae8dfcfc7a3a2c3a00384f48752f
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2021-03-15 17:17:17 +0100

    Primi esperimenti con la classe Polyline e test dell'istanza di Nominatim
    La classe Polyline (https://osmdroid.github.io/osmdroid/javadocAll/org/osmdroid/views/overlay/Polyline.html) permette di inserire segmenti tra più punti (coppie di Latitudine e Longitudine) su di una mappa. Nel contesto dell'applicazione di test, è stato creato un segmento tra la posizione attuale e quella dell'Università di Salerno a Fisciano. I dati sulla posizione di quest'ultima sono stati ricavati dalla mia istanza personale di Nominatim (al momento accessibile solo da rete locale) tramite chiamata alle API REST che restituiscono un JSON

--- LocationTest/app/src/main/java/com/h3rm4nn/locationtest/MainActivity.java
+++ LocationTest/app/src/main/java/com/h3rm4nn/locationtest/MainActivity.java
@@ -42,7 +42,12 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -50,10 +55,14 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.CopyrightOverlay;
 import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.Polyline;
 import org.osmdroid.views.overlay.infowindow.InfoWindow;
 import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
 
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
+import java.util.Properties;
 
 import static com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY;
 
@@ -67,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
     private IMapController mapController;
     private FloatingActionButton button;
     private boolean isServiceStarted = true;
+    private Location myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,6 +107,50 @@ public class MainActivity extends AppCompatActivity {
                         break;
                     case R.id.item3:
                         Toast.makeText(MainActivity.this, "Item3 tapped", Toast.LENGTH_SHORT).show();
+                        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+                        StringRequest request = new StringRequest(Request.Method.GET, "http://192.168.1.22/nominatim/search.php?q=Campus di Fisciano", new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if (myLocation == null) {
+                                    return;
+                                }
+                                if (response.equals("[]")) {
+                                    throw new IllegalStateException("Something's very wrong");
+                                }
+                                System.out.println(response);
+                                JSONArray array = null;
+                                try {
+                                    array = new JSONArray(response);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                                JSONObject obj = null;
+                                try {
+                                    obj = array.getJSONObject(0);
+                                    double latitude = Double.parseDouble(obj.getString("lat"));
+                                    double longitude = Double.parseDouble(obj.getString("lon"));
+                                    GeoPoint unisa = new GeoPoint(latitude, longitude);
+                                    GeoPoint here = new GeoPoint(myLocation);
+                                    Polyline line = new Polyline();
+                                    line.setWidth(12f);
+                                    ArrayList<GeoPoint> points = new ArrayList<>();
+                                    points.add(unisa);
+                                    points.add(here);
+                                    line.setPoints(points);
+                                    map.getOverlays().add(line);
+                                    map.invalidate();
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, "Errore di Volley! " + error.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                        queue.add(request);
                         break;
                 }
                 return true;
@@ -135,6 +189,7 @@ public class MainActivity extends AppCompatActivity {
         locationClient.getCurrentLocation(PRIORITY_HIGH_ACCURACY, cancellationTokenSource.getToken()).addOnSuccessListener(this, new OnSuccessListener<Location>() {
             @Override
             public void onSuccess(Location location) {
+                myLocation = location; //make location object globally available
                 GeoPoint startPoint = new GeoPoint(location.getLatitude(), location.getLongitude());
                 mapController.setCenter(startPoint);
                 Marker m = new Marker(map);
