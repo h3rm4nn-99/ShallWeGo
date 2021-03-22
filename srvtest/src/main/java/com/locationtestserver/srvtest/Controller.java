56ba97b3bf97d5c3e1207b7d6ef200bf4fd2a54c
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2021-03-22 12:37:49 +0100

    Preparazione del server di test per l'implementazione dell'algoritmo genetico
    È stato usato un file json che contiene tutti i Comuni d'Italia con le relative regioni e province di appartenenza. Il codice presente in questo commit mi è servito solo per cominciare a capire il funzionamento del parser per la lettura e la creazione di stringhe json.

--- srvtest/src/main/java/com/locationtestserver/srvtest/Controller.java
+++ srvtest/src/main/java/com/locationtestserver/srvtest/Controller.java
@@ -1,8 +1,20 @@
 package com.locationtestserver.srvtest;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 
 @RestController
 public class Controller {
@@ -10,5 +22,36 @@ public class Controller {
     public String printLocation(@RequestParam("latitude") String latitude, @RequestParam("longitude") String longitude) {
         System.out.println("Latitudine " + latitude + "\nLongitudine " + longitude);
         return "OKAY\nLatitudine " + latitude + "\nLongitudine " + longitude;
+    }
+    @GetMapping("/api/createPeople")
+    public String createPeople() throws IOException, ParseException {
+        JSONParser parser = new JSONParser();
+        JSONArray array = (JSONArray) parser.parse(new FileReader("comuni.json"));
+        Iterator<JSONObject> iterator = array.iterator();
+        JSONArray resultArray = new JSONArray();
+
+        while (iterator.hasNext()) {
+            Random r = new Random();
+            JSONObject obj = iterator.next();
+            JSONObject provinciaCurrent = (JSONObject) obj.get("provincia");
+            String provinciaString = (String) provinciaCurrent.get("nome");
+
+            if (provinciaString.equalsIgnoreCase("Salerno")) {
+                byte[] byteArray = new byte[11];
+                new Random().nextBytes(byteArray);
+                String username = new String(byteArray, StandardCharsets.UTF_8);
+                double karma = r.nextDouble() + r.nextInt(55);
+                int permanenza = r.nextInt(365);
+                String comune = (String) obj.get("nome");
+                UtenteEntity entity = new UtenteEntity(username, comune, karma, permanenza);
+                JSONObject target = new JSONObject();
+                target.put("username", entity.getUserName());
+                target.put("comune", entity.getComune());
+                target.put("karma", entity.getKarma());
+                target.put("permanenza", entity.getPermanenzaSullaPiattaforma());
+                resultArray.add(target);
+            }
+        }
+        return resultArray.toJSONString();
     }
 }
---
+++ srvtest/src/main/java/com/locationtestserver/srvtest/UtenteEntity.java
@@ -0,0 +1,57 @@
+package com.locationtestserver.srvtest;
+
+public class UtenteEntity {
+    private String userName;
+    private String comune;
+    private Double karma;
+    private Integer permanenzaSullaPiattaforma;
+
+    public UtenteEntity(String userName, String comune, Double karma, Integer permanenzaSullaPiattaforma) {
+        this.userName = userName;
+        this.comune = comune;
+        this.karma = karma;
+        this.permanenzaSullaPiattaforma = permanenzaSullaPiattaforma;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    public Double getKarma() {
+        return karma;
+    }
+
+    public void setKarma(Double karma) {
+        this.karma = karma;
+    }
+
+    public Integer getPermanenzaSullaPiattaforma() {
+        return permanenzaSullaPiattaforma;
+    }
+
+    public void setPermanenzaSullaPiattaforma(Integer permanenzaSullaPiattaforma) {
+        this.permanenzaSullaPiattaforma = permanenzaSullaPiattaforma;
+    }
+
+    @Override
+    public String toString() {
+        return "UtenteEntity{" +
+                "userName='" + userName + '\'' +
+                ", comune='" + comune + '\'' +
+                ", karma=" + karma +
+                ", permanenzaSullaPiattaforma=" + permanenzaSullaPiattaforma +
+                '}';
+    }
+}
