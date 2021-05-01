c1f487f18de7db3ce78a445653326f8288734b2c
h3rm4nn-99 <73171414+h3rm4nn-99@users.noreply.github.com>
2021-05-01 14:47:40 +0200

    Revert "Modifica della stringa di query al servizio di Geocoding"
    This reverts commit bb328218c78de7904f70d261e79dac34fa1b7927.

--- ShallWeGoServer/src/main/java/com/locationtestserver/srvtest/logic/entities/User.java
+++ ShallWeGoServer/src/main/java/com/locationtestserver/srvtest/logic/entities/User.java
@@ -18,7 +18,6 @@ public class User implements Serializable {
     private String userName;
     private String password;
     private String comune;
-    private String provincia;
     private Double karma;
     private Integer permanenzaSullaPiattaforma;
 
@@ -27,11 +26,10 @@ public class User implements Serializable {
 
     public User() {}
 
-    public User(String userName, String password, String comune, String provincia, Double karma, Integer permanenzaSullaPiattaforma) {
+    public User(String userName, String password, String comune, Double karma, Integer permanenzaSullaPiattaforma) {
         this.userName = userName;
         this.password = password;
         this.comune = comune;
-        this.provincia = provincia;
         this.karma = karma;
         this.permanenzaSullaPiattaforma = permanenzaSullaPiattaforma;
     }
@@ -76,16 +74,8 @@ public class User implements Serializable {
         this.permanenzaSullaPiattaforma = permanenzaSullaPiattaforma;
     }
 
-    public String getProvincia() {
-        return provincia;
-    }
-
-    public void setProvincia(String provincia) {
-        this.provincia = provincia;
-    }
-
     public double getFitness(Location location) throws IOException, ParseException {
-        double distance = location.distance(this.getComune(), this.getProvincia());
+        double distance = location.distance(this.getComune());
         double distancePartialFitness;
         if (distance == 0) {
             distancePartialFitness = 15 / (distance + 0.1); //avoid division by 0
